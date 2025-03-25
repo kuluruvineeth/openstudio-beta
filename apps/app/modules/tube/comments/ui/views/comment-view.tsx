21cf2720022e2e5ffb629ed12ebf1ca54a961a04
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-03-25 07:06:32 +0530

    ✨ Remove StudioNavbar from AutomationView and CommentView components to streamline layout and improve UI consistency.

--- apps/app/modules/tube/comments/ui/views/comment-view.tsx
+++ apps/app/modules/tube/comments/ui/views/comment-view.tsx
@@ -1,5 +1,4 @@
 import { CommentSection } from '@/modules/tube/comments/ui/sections/comment-section';
-import { StudioNavbar } from '@/modules/tube/studio/ui/components/studio-navbar';
 
 export const CommentView = ({
   organizationId,
@@ -8,7 +7,6 @@ export const CommentView = ({
 }) => {
   return (
     <div className="flex h-screen w-full flex-col">
-      <StudioNavbar></StudioNavbar>
       <div className="flex-1 overflow-auto px-4 pb-8">
         <CommentSection organizationId={organizationId} />
       </div>
